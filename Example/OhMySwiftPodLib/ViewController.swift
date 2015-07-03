757d1519d2342b0a2329b240ed23a9a09ce95c79
ChenYu Xiao <apple.iiiyu@gmail.com>
2015-07-04 01:01:32 +0800

    Initial commit

---
+++ Example/OhMySwiftPodLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OhMySwiftPodLib
+//
+//  Created by ChenYu Xiao on 07/04/2015.
+//  Copyright (c) 2015 ChenYu Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
